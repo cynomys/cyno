7a836dd49adc0008206b478520172b9264356146
Chad Laing <chad.laing@canada.ca>
2019-02-19 15:06:51 -0700

    Initial rust-bio kmer count and insert to dgraph

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use bio::io::fasta;
+
 fn main() {
-    println!("Hello, world!");
+    let reader = fasta::Reader::from_file("/home/chad/ECI-2866.fasta").unwrap();
+    for record in reader.records(){
+        let r = record.unwrap();
+        println!("{:?}", r);
+        break;
+    }
+    println!("Done!");
+
 }
