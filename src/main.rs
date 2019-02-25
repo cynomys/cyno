3c8ab006105e191538a5ca99e884e061b60dbb0c
Chad Laing <chad.laing@canada.ca>
2019-02-25 16:26:31 -0700

    Adding dgraph functionality

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,8 @@ fn main() -> Result<(), std::io::Error> {
     let fs = files::get_fasta_path(Path::new("./data/"))?;
     println!("{:?}", fs);
 
-//    let mut all_kmers: HashMap<String, Vec<genome::ContigKmers>> = HashMap::new();
     let all_kmers = genome::get_kmers_fastas(&fs)?;
 
-    for (k, v) in &all_kmers{
-        println!("{:#?}: {:#?}", k, v);
-        let kx = v[0].get_kmers_contig();
-        println!("{:#?}", kx);
-    }
 
     println!("Done");
     Ok(())
