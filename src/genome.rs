f88a2eeb612afa1e64f485d22f7a2598a3e84b9e
Chad Laing <chad.laing@canada.ca>
2019-02-25 10:14:58 -0700

    Use &str instead of byteslice

--- src/genome.rs
+++ src/genome.rs
@@ -3,6 +3,7 @@ use bio::io::fasta;
 use std::collections::HashMap;
 use std::io::{Error, ErrorKind};
 use std::path::PathBuf;
+use std::str;
 
 #[derive(Debug)]
 pub struct ContigKmers<'a>{
@@ -15,11 +16,13 @@ pub fn get_kmers_fastas<'a>(fs: &Vec<PathBuf>)
     let mut hm = HashMap::new();
 
     // This is the test data
-    let s: &str = "ATCGGCGGCGT";
+    let bs = b"AAATTTCCTTTT";
+    let bs_str = str::from_utf8(bs).unwrap();
 
     let kmer1 = ContigKmers {
         name: String::from("genomeA"),
-        kmers: vec![&s[0..3]]
+//        kmers: vec![]
+        kmers: vec![&bs_str[0..3], &bs_str[4..6]]
     };
 
     hm.insert(String::from("genomeA"), vec![kmer1]);
@@ -30,6 +33,13 @@ pub fn get_kmers_fastas<'a>(fs: &Vec<PathBuf>)
         for record in reader.records(){
             let r = record?;
 
+//            let next_contig = ContigKmers{
+//                name: r.id().to_owned()
+//                // TODO: The bio module has DNA as u8, not str
+//                // kmers: vec![&r.seq().to_vec()[0..3]]
+//            };
+//
+//            hm.insert(ffile.to_string(), vec![next_contig])
         }
     }
 
