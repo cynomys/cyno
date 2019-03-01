2b863fe01ddc68b710bab013ccdda1d1643a5d82
Chad Laing <chad.laing@canada.ca>
2019-03-01 12:23:24 -0700

    Batch Quads creation

--- src/dg.rs
+++ src/dg.rs
@@ -68,9 +68,6 @@ pub fn add_genomes_dgraph(
     let mut kmer_uid: HashMap<String, String> = HashMap::new();
 
     for (k, v) in hm {
-        // Giving an over-allocation of capacity prevent re-allocation later
-        let mut all_quads = String::with_capacity(15000000);
-
         // Iterate through all contigs
         for contig in v {
             // Iterate through all kmers in the contig
@@ -98,24 +95,13 @@ pub fn add_genomes_dgraph(
 
                 // Add new kmers as nodes and edges between them to the graph
                 // Requires a string of newline separated quads
-//                create_batch_quads(&all_quads, &dkmers);
+                let new_quads = create_batch_quads(&dkmers,&kmer_uid, &k);
 //                add_batch_dgraph(client, )
 
 
             }
 
-            //            // Every 1000 kmers, add to dgraph
-            //            if (i > 0) && (i % 30000 == 0){
-            //                add_batch_dgraph(client,&all_quads)?;
-            //                // Empty the string, but leave its capacity the same
-            //                all_quads.clear();
-            //            }
-            //
-            //            // For each pair of kmers, we need to do the following:
-            //            // 1. Add kmer1
-            //            // 2. Add kmer2
-            //            // 3. Add an edge (genome name from schema) between them
-            //            println!("There are {} kmers", all_kmers.len());
+
             //
             ////            for i in 0..(all_kmers.len() - 2) {
             //            for i in 0..50000{
@@ -154,6 +140,60 @@ pub fn add_genomes_dgraph(
 
     Ok(())
 }
+
+
+// Create all the quads we need
+fn create_batch_quads<'a>(kmers: &Vec<&str>, hm: &HashMap<String, String>, genome_name: &str) -> Vec<String>{
+    let mut new_quads = Vec::new();
+
+    for i in 0..kmers.len() - 2{
+        let k1_uid = upsert_uid(hm, kmers[i]);
+        let k2_uid = upsert_uid(hm, kmers[i+1]);
+
+        let mut k1_node = String::with_capacity(1 + k1_uid.len() + " <kmer> ".len() + kmers[i].len() + " .".len());
+        k1_node.push_str(&k1_uid);
+        k1_node.push_str(" <kmer> ");
+        k1_node.push_str(kmers[i]);
+        k1_node.push_str(" .");
+        new_quads.push(k1_node.to_owned());
+
+        let mut k2_node = String::with_capacity(1 + k2_uid.len() + " <kmer> ".len() + kmers[i+1].len() + " .".len());
+        k2_node.push_str(&k2_uid);
+        k2_node.push_str(" <kmer> ");
+        k2_node.push_str(kmers[i+1]);
+        k2_node.push_str(" .");
+        new_quads.push(k2_node.to_owned());
+
+        let mut edge = String::with_capacity(1 + k1_uid.len() + genome_name.len() + "<>".len() + k2_uid.len() + " .".len());
+        edge.push_str(&k1_uid);
+        edge.push_str("<");
+        edge.push_str(genome_name);
+        edge.push_str(">");
+        edge.push_str(&k2_uid);
+        edge.push_str(" .");
+        new_quads.push(edge.to_owned());
+    };
+
+    new_quads
+}
+
+
+// Use the HashMap as the db for an "upsert" of the uid
+fn upsert_uid(hm: & HashMap<String, String>, k: &str) -> String{
+    // Check to see if kmer is already in the graph
+    // If it is, grab the uid, if not, use a blank node
+    match hm.get(k){
+        Some(k) => k.to_owned(),
+        None => {
+            // If we pre-allocate the string-size, building it is much more efficient
+            let mut uid = String::with_capacity(4 + k.len());
+            uid.push_str("_:k");
+            uid.push_str(k);
+            uid
+        }
+    }
+}
+
 
 // Batch add the kmers,
 fn add_batch_dgraph(client: &Dgraph, nq: &str) -> Result<(), Error> {
