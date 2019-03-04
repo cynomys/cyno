9a56c8bc793a2e43555cf4ff8cb619ef69199e90
Chad Laing <chad.laing@canada.ca>
2019-03-04 11:06:42 -0700

    Renamed genome parsing

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,14 @@ fn main() -> Result<(), std::io::Error> {
     let fs = files::get_fasta_path(Path::new("./data/E_coli.fasta"))?;
     println!("{:?}", fs);
 
-    let all_kmers = genome::get_parsed_genomes(&fs, 11)?;
+    let parsed_genomes = genome::get_parsed_genomes(&fs, 11)?;
 
     // dgraph init
     let dg_client = dg::create_dgraph_connection("10.139.14.202:9080")?;
     dg::drop_all(&dg_client)?;
 
     dg::set_schema(&dg_client);
-    dg::add_genomes_dgraph(&dg_client, &all_kmers, 5000000)?;
+    dg::add_genomes_dgraph(&dg_client, &parsed_genomes, 5000000)?;
 
     println!("Done");
     Ok(())
