dd3cc1effb1e353a19822a95fe8f8e849e687daa
Chad Laing <chad.laing@canada.ca>
2019-03-07 12:22:03 -0700

    Testing Salmonella single genome on linux

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,13 @@ mod genome;
 use std::path::Path;
 
 fn main() -> Result<(), std::io::Error> {
-    let fs = files::get_fasta_path(Path::new("./data/E_coli.fasta"))?;
+    let fs = files::get_fasta_path(Path::new("./data/Salmonella.fasta"))?;
     println!("{:?}", fs);
 
     let parsed_genomes = genome::get_parsed_genomes(&fs, 11)?;
 
     // dgraph init
-    let dg_client = dg::create_dgraph_connection("10.139.14.202:9080")?;
+    let dg_client = dg::create_dgraph_connection("10.139.14.193:9080")?;
     dg::drop_all(&dg_client)?;
 
     dg::set_schema(&dg_client)?;
